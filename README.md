a845450f11b998288114fbac8d823e2f2a4706ab
francis iduh <fiduh987@gmail.com>
2024-08-25 15:05:38 +0100

    update README.md,iam/iam.tf

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 1. S3 bucket with IAM policy to make it public or private - T1 (Basic + Module)
 2. Create IAM user and assign groups - In progress (T2 Basic)
-3. Assign a role to an IAM group (T2 Basic)
-    -    3.1 Assign a role to a IAM user (T2 Basic)
+3. Assign a policy to an IAM group (T2 Basic)
+    -    3.1 Assign a policy to a IAM user (T2 Basic)
 4. Create IAM role/policy a resource EC2 - Complete
 5. Assign of IAM role during EC2 resource creation - In Progress (T2 Basic)
 6. Create Lambda with a role premade (T2 Basic)
--- iam/iam.tf
+++ iam/iam.tf
@@ -1,40 +1,80 @@
+# Provider Configuration
 provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_iam_policy_document" "assume_role" {
+# Resource 1: IAM Group
+resource "aws_iam_group" "developers" {
+  name = "developers"  # Name of the IAM group
+  path = "/users/"     # Path for the IAM group in AWS
+}
+
+# Resource 2: IAM User
+resource "aws_iam_user" "dev1" {
+  name          = "dev1"          # Name of the IAM user
+  path          = "/users/"       # Path for the IAM user in AWS
+  force_destroy = true            # Allow Terraform to delete the user even if it has non-terraform-managed resources like access keys
+}
+
+# Resource 3: IAM User Login Profile
+resource "aws_iam_user_login_profile" "this" {
+  user                    = aws_iam_user.dev1.name  # Associate the login profile with the user created above
+  password_reset_required = true                    # Require the user to reset their password upon first login
+}
+
+# Resource 4: IAM User Group Membership
+resource "aws_iam_user_group_membership" "this" {
+  user   = aws_iam_user.dev1.name                  # IAM user to be added to the group
+  groups = [aws_iam_group.developers.name]         # Group(s) the user should be a member of
+}
+
+# Data Source 1: IAM Policy Document
+data "aws_iam_policy_document" "developer_policy" {
   statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
+    effect    = "Allow"                            # Policy effect, in this case allowing actions
+    actions   = [                                  # List of actions this policy allows
+      "ec2:*",                                     # All actions for EC2 service
+      "dynamodb:*"                                 # All actions for DynamoDB service
+    ]
+    resources = ["*"]                              # Applies the policy to all resources
   }
 }
 
-resource "aws_iam_role" "role" {
-  name               = "ec2-s3-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+# Resource 5: IAM Group Policy
+resource "aws_iam_group_policy" "group_developer_policy" {
+  name   = "group_developer_policy"                # Name of the IAM group policy
+  group  = aws_iam_group.developers.name           # Attach the policy to the developers group
+  policy = data.aws_iam_policy_document.developer_policy.json  # The policy document from the data source
 }
 
-data "aws_iam_policy_document" "policy" {
+
+################################################################
+# Create User and Policy
+################################################################
+# Resource 1: IAM User
+resource "aws_iam_user" "dev2" {
+  name = "dev2"  # Name of the IAM user
+}
+
+# Resource 2: IAM Access Key
+resource "aws_iam_access_key" "dev2" {
+  user = aws_iam_user.dev2.name  # Access key for the IAM user "dev2"
+}
+
+# Data Source 1: IAM Policy Document
+data "aws_iam_policy_document" "dev2" {
   statement {
-    effect    = "Allow"
-    actions   = ["s3:*"]
-    resources = ["*"]
+    effect    = "Allow"          # Policy effect, in this case allowing actions
+    actions   = [                # List of actions this policy allows
+      "ec2:*"                    # All actions for EC2 service
+    ]
+    resources = ["*"]            # Applies the policy to all resources
   }
 }
 
-resource "aws_iam_policy" "policy" {
-  name        = "s3-policy"
-  description = "A test S3 policy"
-  policy      = data.aws_iam_policy_document.policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.policy.arn
+# Resource 3: IAM User Policy
+resource "aws_iam_user_policy" "dev2_policy" {
+  name   = "test"                            # Name of the IAM user policy
+  user   = aws_iam_user.dev2.name            # Attach the policy to the IAM user "dev2"
+  policy = data.aws_iam_policy_document.dev2.json  # The policy document from the data source
 }
