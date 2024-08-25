f4620aa8aa4805ed5de7421540abf501de4d6f4a
francis iduh <fiduh987@gmail.com>
2024-08-25 16:15:21 +0100

    update lambda/lambda.tf

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -2,31 +2,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# IAM Role for Lambda
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda-execution-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   ]
+
 }
+
 
 # Lambda Function
 resource "aws_lambda_function" "this" {
-  function_name = "HelloWorldFunction"
+  function_name = "lambdaFunction"
   handler       = "index.handler"
   runtime       = "nodejs20.x"
 
@@ -34,3 +37,31 @@ resource "aws_lambda_function" "this" {
   filename      = "lambda_function.zip"
   source_code_hash = filebase64sha256("lambda_function.zip")
 }
+
+
+
+#   data "aws_iam_policy_document" "lambda_logging" {
+#   statement {
+#     effect = "Allow"
+
+#     actions = [
+#       "logs:CreateLogGroup",
+#       "logs:CreateLogStream",
+#       "logs:PutLogEvents",
+#     ]
+
+#     resources = ["*"]
+#   }
+# }
+
+# resource "aws_iam_policy" "lambda_logging" {
+#   name        = "lambda_logging"
+#   path        = "/"
+#   description = "IAM policy for logging from a lambda"
+#   policy      = data.aws_iam_policy_document.lambda_logging.json
+# }
+
+# resource "aws_iam_role_policy_attachment" "lambda_logs" {
+#   role       = aws_iam_role.lambda_role.name
+#   policy_arn = aws_iam_policy.lambda_logging.arn
+# }
