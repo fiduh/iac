29b657ffebd1fd2af83822004e1879b74851856d
Ace Kloud-Native <fiduh987@gmail.com>
2024-10-04 20:52:56 +0000

    update http api hasicorp module

--- http_api_gateway/http_api_gateway_hasicorp_module.tf
+++ http_api_gateway/http_api_gateway_hasicorp_module.tf
@@ -1,8 +1,18 @@
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  lambda_arn = "arn:aws:lambda:us-east-1:426991683772:function:demoNodeJS"
+}
+
+
+
 module "api_gateway" {
   source = "terraform-aws-modules/apigateway-v2/aws"
 
   name          = "dev-http"
-  description   = "My awesome HTTP API Gateway"
+  description   = "Ticketing HTTP API Gateway"
   protocol_type = "HTTP"
 
   cors_configuration = {
@@ -11,80 +21,60 @@ module "api_gateway" {
     allow_origins = ["*"]
   }
 
-  # Custom domain
-  domain_name = "terraform-aws-modules.modules.tf"
 
-  # Access logs
-  stage_access_log_settings = {
-    create_log_group            = true
-    log_group_retention_in_days = 7
-    format = jsonencode({
-      context = {
-        domainName              = "$context.domainName"
-        integrationErrorMessage = "$context.integrationErrorMessage"
-        protocol                = "$context.protocol"
-        requestId               = "$context.requestId"
-        requestTime             = "$context.requestTime"
-        responseLength          = "$context.responseLength"
-        routeKey                = "$context.routeKey"
-        stage                   = "$context.stage"
-        status                  = "$context.status"
-        error = {
-          message      = "$context.error.message"
-          responseType = "$context.error.responseType"
-        }
-        identity = {
-          sourceIP = "$context.identity.sourceIp"
-        }
-        integration = {
-          error             = "$context.integration.error"
-          integrationStatus = "$context.integration.integrationStatus"
-        }
-      }
-    })
-  }
-
-  # Authorizer(s)
-  authorizers = {
-    "azure" = {
-      authorizer_type  = "JWT"
-      identity_sources = ["$request.header.Authorization"]
-      name             = "azure-auth"
-      jwt_configuration = {
-        audience         = ["d6a38afd-45d6-4874-d1aa-3c5c558aqcc2"]
-        issuer           = "https://sts.windows.net/aaee026e-8f37-410e-8869-72d9154873e4/"
-      }
-    }
-  }
 
   # Routes & Integration(s)
-  routes = {
-    "POST /" = {
+    routes = {
+
+     "ANY /api/auth/{x}" = {
+      detailed_metrics_enabled = false
+
       integration = {
-        uri                    = "arn:aws:lambda:eu-west-1:052235179155:function:my-function"
+        uri = local.lambda_arn
         payload_format_version = "2.0"
-        timeout_milliseconds   = 12000
       }
     }
 
-    "GET /some-route-with-authorizer" = {
-      authorizer_key = "azure"
-
+    "ANY /api/ticket" = {
       integration = {
-        type = "HTTP_PROXY"
-        uri  = "some url"
+        uri = local.lambda_arn
+        type            = "AWS_PROXY"
+        payload_format_version = "1.0"
       }
     }
 
-    "$default" = {
+    "ANY /api/ticket/status" = {
       integration = {
-        uri = "arn:aws:lambda:eu-west-1:052235179155:function:my-default-function"
+        uri = local.lambda_arn
+        payload_format_version = "1.0"
+      }
+    }
+
+    "ANY /api/ticket/archive" = {
+      integration = {
+        uri = local.lambda_arn
+        type            = "AWS_PROXY"
+        payload_format_version = "1.0"
+      }
+    }
+
+    "ANY /api/ticket/reply-email" = {
+      integration = {
+        uri = local.lambda_arn
+        type            = "AWS_PROXY"
+        payload_format_version = "1.0"
+      }
+    }
+
+    "ANY /api/ticket/replyticket" = {
+      integration = {
+        uri = local.lambda_arn
+        type            = "AWS_PROXY"
+        payload_format_version = "1.0"
       }
     }
   }
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+
+
 }
